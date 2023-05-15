e0d3e1b5c772abc10a8906b1ed9a51094b251066
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-15 22:04:56 +0530

    to import ExpenseItem

---
+++ App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import ExpenseItem from './ExpenseItem';
+// import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Lets Start here</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+export default App;
