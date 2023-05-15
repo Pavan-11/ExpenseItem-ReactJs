c0cc0eedf354fc827095489d07a247ca3fdf5535
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-15 22:03:47 +0530

    Expense Item Level 1
    created a basic expense Item list

---
+++ ExpenseItem.js
@@ -0,0 +1,22 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div><h2>Expense Item</h2></div>
+            <div>
+                <h2>Food Item</h2>
+                <div>Rs 10</div>
+            </div>
+            <div>
+                <h2>Petrol</h2>
+                <div>Rs 1000</div>
+            </div>
+            <div>
+                <h2>Movie</h2>
+                <div>Rs 200</div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ExpenseItem;
