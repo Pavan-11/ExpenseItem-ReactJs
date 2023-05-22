07e546218d368a22209d07ca944f3fe246ac8591
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-22 16:00:33 +0530

    change Expense amount
    using useState created a function to change the title and amount when user clicks on buttons

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -7,24 +7,28 @@ import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (data) => {
     const [title, setTitle] = useState(data.title);
+    const [amount, setAmount] = useState(data.amount);
 
-    
 
 
     function clickHandler(){
         setTitle('Updated');
         console.log(title)
     };
+    function changeExpense(){
+        setAmount('100')
+        console.log(amount)
+    } 
     return (
         <div className="expense-item">
             <ExpenseDate date={data.date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
-                <div className='expense-item__price'>${data.amount}</div>
+                <div className='expense-item__price'>${amount}</div>
             </div>
             {/* <ExpenseDetails title = {data.title} amount = {data.amount} location = {data.location}/> */}
             <button onClick={clickHandler}>Change title</button>
-            {/* <button onClick={deleteExpense}>Delete Expense</button> */}
+            <button onClick={changeExpense}>Change Expense</button>
         </div>
 
     );
