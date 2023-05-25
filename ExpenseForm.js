8fa7af758800a4671cdd56e27f4d782236554c23
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-25 13:30:50 +0530

    Submittiing form data using javascript

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseForm.css';
 
 
 function ExpenseForm(){
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('')
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle : '',
+    //     enteredAmount : '',
+    //     enteredDate : ''
+    // })
+
+
     const titleChangerHandler = (event) => {
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
+    }
+    const amountChangerHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
+    }
+    const dateChangerHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // })
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title : enteredTitle,
+            amount : enteredAmount,
+            date : new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+
     }
 
 
 
 
-    return <form>
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
@@ -18,11 +60,11 @@ function ExpenseForm(){
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min='0.01' step='0.01'></input>
+                <input type="number" min='0.01' step='0.01' onChange={amountChangerHandler}></input>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min='2019-01-01' max="2023-12-31"></input>
+                <input type="date" min='2019-01-01' max="2023-12-31" onChange={dateChangerHandler}></input>
             </div>
             <div className="new-expense__control">
                 <label>Location</label>
