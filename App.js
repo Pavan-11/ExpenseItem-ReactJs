37a9055678bd1ab1d93eb617010749ec3505bdf5
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-25 22:22:10 +0530

    Making the expense form input visible on screen

--- App.js
+++ App.js
@@ -1,52 +1,47 @@
-// import logo from './logo.svg';
-import ExpenseItem from './components/Expenses/ExpenseItem';
-// import './App.css';
-import './index.css';
+import React from 'react';
+import NewExpense from './components/New Expenses/NewExpense';
+import Expenses from './components/Expense/Expenses';
 
 const App = () => {
   const expenses = [
     {
-      id : 'e1',
-      title : 'Toilet Paper',
-      amount : 100,
-      date : new Date(2020, 7, 14),
-      LocationOfPurchase : 'Andhra Pradesh'
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id : 'e2',
-      title : 'Car insurance',
-      amount : 200,
-      date : new Date(2021 , 2 , 28),
-      LocationOfPurchase : 'GOA'
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
-    {
-      id : 'e3',
-      title : 'New Tv',
-      amount : 500,
-      date : new Date(2023 , 5, 15),
-      LocationOfPurchase : 'Uttar Pradesh'
-    },
-    {
-      id : 'e4',
-      title : 'New Phone',
-      amount : 1000,
-      date : new Date(2024 , 6, 15),
-      LocationOfPurchase : 'Himachal Pradesh'
-    }
-  ]
-  return (
-    <div>
-      {expenses.map((e) =>{
-        return (
-          <ExpenseItem title = {e.title} amount = {e.amount} date = {e.date} location = {e.LocationOfPurchase}></ExpenseItem>
-        )
-      })}
-      {/* <h2>Lets Start here</h2> */}
-     {/* <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date} location = {expenses[0].LocationOfPurchase}></ExpenseItem>
-     <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date} location = {expenses[1].LocationOfPurchase}></ExpenseItem>
-     <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date} location = {expenses[2].LocationOfPurchase}></ExpenseItem>
-     <ExpenseItem title = {expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date} location = {expenses[3].LocationOfPurchase}></ExpenseItem> */}
-    </div>
-  );
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense);
+  }
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  //   );
+
+ return (
+   <div>
+     <NewExpense onAddExpense = {addExpenseHandler}/>
+     <Expenses items={expenses} />
+   </div>
+);
 }
+
 export default App;
