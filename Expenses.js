d51d41fbbf8cd6424c31c2d81edf1f8f97169cc2
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-27 17:05:43 +0530

    using key in expenses.js
    Adding key ,using useSate,making the values dynamic

--- Expenses.js
+++ Expenses.js
@@ -13,29 +13,34 @@ const Expenses = (props) => {
     };
 
   return (
-    <Card className="expenses">
-      <ExpenseFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler}></ExpenseFilter>
-      <ExpenseItem
+    <div>
+
+
+      <Card className="expenses__card">
+        <ExpenseFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler}></ExpenseFilter>
+        {props.items.map(expense => <ExpenseItem title= {expense.title} amount = {expense.amount} date={expense.date} location = {expense.location}/>)}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
-      />
-      <ExpenseItem
+        />
+        <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
-      />
-      <ExpenseItem
+        />
+        <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
         date={props.items[2].date}
-      />
-      <ExpenseItem
+        />
+        <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
-    </Card>
+      /> */}
+      </Card>
+    </div>
   );
 }
 
