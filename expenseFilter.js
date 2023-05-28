fbbd717a244b227581c4f36e034cb82c889fb197
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-05-28 19:09:47 +0530

    Update expenseFilter.js

--- expenseFilter.js
+++ expenseFilter.js
@@ -12,9 +12,9 @@ const ExpenseFilter = (props) =>{
                 <label>Filter by Year</label>
                 <select value={props.selected} onChange={dropDownChangeHandler}>
                     <option value = '2022'>2022</option>
-                    <option value = '2022'>2021</option>
-                    <option value = '2022'>2020</option>
-                    <option value = '2022'>2019</option>
+                    <option value = '2021'>2021</option>
+                    <option value = '2020'>2020</option>
+                    <option value = '2019'>2019</option>
                 </select>
             </div>
         </div>
